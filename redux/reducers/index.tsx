d3b9e24276313d5cf030b43d1ca8063d0b46d811
bataimx <bataimx@gmail.com>
2022-03-31 21:17:26 +0700

    1 file modified

--- redux/reducers/index.tsx
+++ redux/reducers/index.tsx
@@ -2,12 +2,9 @@ import SettingReducer from './SettingReducer';
 import PhotoListReducer from './PhotoListReducer';
 import CollectionsReducer from './CollectionsReducer';
 import { StoreModel } from '../../models';
-import { reducerList } from './util';
 
-const rootReducer: StoreModel = {
+export const rootReducer: StoreModel = {
   Setting: SettingReducer,
   Photos: PhotoListReducer,
   Collections: CollectionsReducer,
 };
-
-export { rootReducer };
